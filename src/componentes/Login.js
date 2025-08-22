0107c8cf3ddab05c496e5d68ec4420fdc1a1748b
KaiSser <tuanhiep29797@gmail.com>
2025-08-22 19:04:54 +0700

    update login information

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -5,7 +5,10 @@ const Login = () => {
   const navigate = useNavigate();
 
   const onFinish = (values) => {
-    if (values.username === "a" && values.password === "1") {
+    if (
+      values.username === "user@gmail.com" &&
+      values.password === "password123"
+    ) {
       if (values.remember === true) {
         localStorage.setItem("username", "user@gmail.com");
         localStorage.setItem("password", "password123");
@@ -20,45 +23,43 @@ const Login = () => {
   return (
     <div className="container">
       <div className="login">
-      <Divider style={{ fontSize: 50, margin: 20 }}>
-        LOGIN
-      </Divider>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        autoComplete="off"
-        className="formLogin"
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
+        <Divider style={{ fontSize: 50, margin: 20 }}>LOGIN</Divider>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
+          className="formLogin"
         >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item name="remember" valuePropName="checked" label={null}>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" label={null}>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-        <Form.Item label={null}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item label={null}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   );
